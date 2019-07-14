4fa3b26d558a8a9e5a305cebe5b4e59bc3346254
Hector Blanco <hectorblanco83@gmail.com>
2019-07-14 21:00:28 +0200

    Added log

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,6 +1,8 @@
 package com.mobiquityinc.packer;
 
 import com.mobiquityinc.packer.exception.APIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -11,7 +13,21 @@ import com.mobiquityinc.packer.exception.APIException;
  */
 public class Packer {
 	
+	// LOGGER
+	private static final Logger LOGGER = LoggerFactory.getLogger(Packer.class);
+	
+	
+	/**
+	 * Reads the input file e calculate the packages, returning a String with the thing's indexes that will be
+	 * in the package, separated by commas. Each "row" in the String (separated by System.lineSeparator) indicates
+	 * a "package" in the input file
+	 *
+	 * @param filePath path of the input file
+	 * @return The string that represents the optimized package
+	 * @throws APIException in case of any error during package creation
+	 */
 	public static String pack(String filePath) throws APIException {
+		LOGGER.debug("Input file path is: {}", filePath);
 		return "TODO";
 	}
 	
